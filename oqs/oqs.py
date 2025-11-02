f2619fe2c76d1888f7c2515da6fb15fd9f95bba4
Guiliano99 <guilianolehmann@live.de>
2025-11-02 08:18:34 +0100

    Fix Faulthandler import error for RobotFramework (RF).
    - run: ruff format
    
    Signed-off-by: Guiliano99 <guilianolehmann@live.de>

--- oqs/oqs.py
+++ oqs/oqs.py
@@ -17,9 +17,19 @@ import platform  # to learn the OS we're on
 import subprocess
 import tempfile  # to install liboqs on demand
 import time
-import faulthandler
+import os
 
-faulthandler.enable()
+# To identify issues in native code, we enable faulthandler.
+# As an example, this will print a stack trace if a segfault occurs,
+# if the STFL key generation flag was not set when building liboqs.
+if os.environ.get("PYOQS_ENABLE_FAULTHANDLER", "0") == "1":
+    import faulthandler
+
+    faulthandler.enable()
+    logging.info("liboqs-python faulthandler is enabled")
+else:
+    logging.info("liboqs-python faulthandler is disabled")
+
 
 try:
     import tomllib  # Python 3.11+
@@ -111,9 +121,7 @@ def _load_shared_obj(
             elif platform.system() == "Windows":
                 # Try both oqs.dll and liboqs.dll in the install path
                 for dll_name in (name, f"lib{name}"):
-                    paths.append(
-                        path.absolute() / Path(dll_name).with_suffix(".dll")
-                    )
+                    paths.append(path.absolute() / Path(dll_name).with_suffix(".dll"))
             else:  # Linux/FreeBSD/UNIX
                 paths.append(path.absolute() / Path(f"lib{name}").with_suffix(".so"))
                 # https://stackoverflow.com/questions/856116/changing-ld-library-path-at-runtime-for-ctypes
